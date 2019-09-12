795d9e50322cc0c22dca4e9416d81987617f7781
xzho2604 <erikxiangzhou@gmail.com>
2019-09-12 21:42:05 +1000

    first

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,8 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from scipy.spatial import KDTree
+import numpy as np
 
 import math
 
@@ -29,6 +31,8 @@ class WaypointUpdater(object):
         rospy.init_node('waypoint_updater')
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        # basic_waypoints are static and will not change so will 
+        # be loaded only subscribed only once
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
@@ -37,15 +41,76 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
 
-        rospy.spin()
+        # rospy.spin()
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                # get the closest waypoints
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+
+    def get_closest_waypoint_idx(self):
+        # get the currenty position of the car
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+
+        # find the index of the points that is cloese to the current car position 
+        # we ask to return 1 closet point KDTree returns the position as well as the index of
+        # the cloese waypoint, index would be the same order as we construct the KDTree
+        closest_idx = self.waypoint_tree.query([x,y],1)[1]
+
+        # check the closet way point is ahead or behind the car
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx-1]
+
+        # equation for hyperplane through closest_coords
+        # test to see if the prev_vect and cl_vect are the same direction as 
+        # from current to cl_vect
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x,y])
+
+        val = np.dot(cl_vect-prev_vect,pos_vect-cl_vect)
+
+        if val < 0: # the direction is opposite the closet point is behind the car
+            # we need to use the next points insteand , modula len to wrap around
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+    def publish_waypoints(self, closest_idx):
+        lane = Lane()
+        lane.header = self.base_waypoints.header # same format we do not need hearder anyway
+        # no need to worry about the greater than the base waypoint len since python slice will
+        # just slice to the end if the lengths is greater 
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx: closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
+
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        self.pose = msg # store the car's pose about 50hz
+
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            # just to get the coordinates of the waypoints (x,y)
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] \
+                                 for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d) # constructa KDTree using the 2d waypoints
+
+
+
         pass
 
     def traffic_cb(self, msg):
